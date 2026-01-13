d23587e28f35d5652cbea2f353c3bc1e0570a8b1
kye <libbykye@gmail.com>
2026-01-13 10:26:56 -0700

    Defined log levels

--- Logger.h
+++ Logger.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <string>
+
+enum class LogLevels {
+	info,
+	warn,
+	error,
+	detected,
+};
+
+class Logger
+{
+
+public:
+
+	static void info(const std::string& message);
+	static void warn(const std::string& message);
+	static void error(const std::string& message);
+	static void detected(const std::string& message);
+
+
+private:
+
+	static void write(LogLevels level, const std::string& message );
+
+	static std::string logLevelToText(LogLevels level);
+
+};
